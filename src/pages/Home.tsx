1291f8adb2df6f1f77a4b7b223a6a785409b846c
Computurtle <44824998+Computurtle@users.noreply.github.com>
2025-04-16 07:32:26 +0800

    remove youtube link

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -54,17 +54,6 @@ function HomePage() {
             alt="itch logo"
           />
         </a>
-        <a
-          href="https://www.youtube.com/@Computurtle"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src="https://img.shields.io/static/v1?message=Youtube&logo=youtube&label=&color=FF0000&logoColor=white&labelColor=&style=for-the-badge"
-            height="30"
-            alt="youtube logo"
-          />
-        </a>
       </div>
 
       <hr />
